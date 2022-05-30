a53c213a1d0289f3c6a03fda47432b3e378e6ade
christian29200 <lefatoum@gmail.com>
2022-05-30 14:27:59 +0200

    gitignore

--- psg_news/news/views.py
+++ psg_news/news/views.py
@@ -1,13 +1,18 @@
+
 from django.shortcuts import render
 import requests
 import json
 import config
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+api_key = os.getenv("api_key")
 # Create your views here.
 
 
 def home_view(request):
-    url = "https://newsapi.org/v2/top-headlines?q=psg&category=sport&country=fr" + api_key
+    url = f"https://newsapi.org/v2/top-headlines?q=psg&category=sport&country=fr{ api_key}"
     response = requests.get(url)
     content_from_internet = json.loads(response.content)
     context={
@@ -17,7 +22,7 @@ def home_view(request):
 
 
 def usa_view(request):
-    url = "https://newsapi.org/v2/top-headlines?country=us"+ api_key
+    url = f"https://newsapi.org/v2/top-headlines?country=us{api_key}"
     response = requests.get(url)
     content_from_internet = json.loads(response.content)
     context={
@@ -29,7 +34,7 @@ def usa_view(request):
 
 
 def world_view(request):
-    url = "https://newsapi.org/v2/top-headlines?language=en"+ api_key
+    url = f"https://newsapi.org/v2/top-headlines?language=en{api_key}"
     response = requests.get(url)
     content_from_internet = json.loads(response.content)
     context={
@@ -39,7 +44,7 @@ def world_view(request):
 
 
 def france_view(request):
-    url = "https://newsapi.org/v2/top-headlines?country=fr"+ api_key
+    url = f"https://newsapi.org/v2/top-headlines?country=fr{api_key}"
     response = requests.get(url)
     content_from_internet = json.loads(response.content)
     context={
@@ -49,7 +54,7 @@ def france_view(request):
 
 
 def necro_view(request):
-    url = "https://newsapi.org/v2/top-headlines?q=died" + api_key
+    url = f"https://newsapi.org/v2/top-headlines?q=died" + api_key
     response = requests.get(url)
     content_from_internet = json.loads(response.content)
     context={
